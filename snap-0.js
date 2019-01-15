e1d5ec92e45dfb68658c2c50e5c62119db95ee0a
Adian Tsosie <addve12@yahoo.com>
2019-01-15 08:53:32 -0700

    Snap challenge O

---
+++ snap-0.js
@@ -0,0 +1,17 @@
+{
+let x =2;
+
+function addNumbers (n, x) {
+	console.log(x);
+	return n + x;
+}
+
+addNumbers(1, 2);
+}
+
+function myFunction() {
+	var str1 = "Good morning";
+	var str2 = "you all";
+	var res = str1.concat(str2);
+
+}
